97231e6e8858e632639fac07167821a2b16c2f2e
DuveD <davidmarinme@gmail.com>
2025-09-15 01:21:56 +0200

    Añadidos idiomas de pistas de audio y doblaje. Vista de capítulo de forma gráfica. Correcciones menores y mejoras de claridad del código.

---
+++ UtilidadesString.js
@@ -0,0 +1,19 @@
+// Función para obtener el texto en small caps.
+function toSmallCaps(str) {
+  const map = {
+    a: 'ᴀ', b: 'ʙ', c: 'ᴄ', d: 'ᴅ', e: 'ᴇ', f: 'ꜰ', g: 'ɢ',
+    h: 'ʜ', i: 'ɪ', j: 'ᴊ', k: 'ᴋ', l: 'ʟ', m: 'ᴍ', n: 'ɴ',
+    o: 'ᴏ', p: 'ᴘ', q: 'ǫ', r: 'ʀ', s: 's', t: 'ᴛ',
+    u: 'ᴜ', v: 'ᴠ', w: 'ᴡ', x: 'x', y: 'ʏ', z: 'ᴢ'
+  };
+
+  return str
+    .toLowerCase()
+    .split('')
+    .map(c => map[c] || c)
+    .join('');
+}
+
+module.exports = {
+    toSmallCaps
+};
